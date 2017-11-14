4e7387cca8f1dcca12f82363dfe944965c45b25c
jwlh <jonnyhall@me.com>
2017-11-14 14:04:40 +0000

    added different icons for each event

--- controllers/proxies.js
+++ controllers/proxies.js
@@ -2,7 +2,7 @@ const rp = require('request-promise');
 
 
 function getEvents(req, res) {
-  rp(`http://api.eventful.com/json/events/search?app_key=xZsJsxZbnKn2phn3&where=51.507602,-0.127816&within=10&date=today&page_size=25&sort_order=popularity&include=popularity&offset=${req.params.offset}`)
+  rp(`http://api.eventful.com/json/events/search?app_key=xZsJsxZbnKn2phn3&where=51.507602,-0.127816&within=10&date=today&page_size=25&sort_order=popularity&include=popularity,categories&offset=${req.params.offset}&category=music,comedy,festivals_parades,performing_arts,sports`)
     .then(data => {
       // console.log(data);
       return res.status(200).json(JSON.parse(data));
--- src/js/directives/googleMap.js
+++ src/js/directives/googleMap.js
@@ -9,6 +9,7 @@ let markers = [];
 let map = null;
 let infowindow = null;
 let APIOffset = 0;
+let eventIcon = null;
 
 googleMap.$inject = ['$window', '$http', 'API'];
 function googleMap($window, $http, API) {
@@ -45,19 +46,62 @@ function googleMap($window, $http, API) {
 
             events.forEach((event) => {
               console.log(event);
+              setIcon(event);
               addMarker(event);
             });
           });
       }
 
+
+      function setIcon(event) {
+        //get the type of event from API
+        const type = event.categories.category[0].id;
+
+        var icons = {
+          sport: {
+            icon: 'images/sport_pointer.png'
+          },
+          music: {
+            icon: 'images/concert_pointer.png'
+          },
+          performing_arts: {
+            icon: 'images/theatre_pointer.png'
+          },
+          comedy: {
+            icon: 'images/comedy_pointer.png'
+          },
+          general: {
+            icon: 'images/blank_pointer.png'
+          }
+        };
+        //if event is music assign music logo
+        if (type === 'sport') {
+          eventIcon = icons.sport;
+        } else if (type === 'music') {
+          eventIcon = icons.music;
+        } else if  (type === 'performing_arts') {
+          eventIcon = icons.performing_arts;
+        } else if  (type === 'comedy') {
+          eventIcon = icons.comedy;
+        } else if  (type === 'family_fun_kids') {
+          eventIcon = icons.performing_arts;
+        }  else if  (type === 'festivals_parades') {
+          eventIcon = icons.music;
+        } else {
+          eventIcon = icons.general;
+        }
+
+        //if event is theatre assign theatre logo
+      }
       function addMarker(event){
 
         const latLng = { lat: parseFloat(event.latitude), lng: parseFloat(event.longitude) };
 
+        console.log(eventIcon);
         const marker = new google.maps.Marker({
           position: latLng,
           map: map,
-          icon: 'images/dot.svg'
+          icon: eventIcon.icon
         });
 
         // var marker = new google.maps.Circle({
@@ -89,6 +133,7 @@ function googleMap($window, $http, API) {
             <h3>Event Start Time:${event.start_time}</h3>
             <h3>Event Finish Time:${event.stop_time}</h3>
             <h3>Popularity Score:${event.popularity}</h3>
+            <h3>Event Type:${event.categories.category[0].id}</h3>
             <a>Show More</a>
           </div>
           `
