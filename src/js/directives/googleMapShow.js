19cda631d4174712f275bc6dafea58701afbd319
sarahalpay <sarah.g.alpay@gmail.com>
2017-11-16 21:32:50 +0000

    reviews page styling

--- src/js/directives/googleMapShow.js
+++ src/js/directives/googleMapShow.js
@@ -5,27 +5,40 @@ function googleMapShow($window, $timeout) {
   return {
     restrict: 'E',
     replace: true,
-    template: '<div class="google-map"></div>',
+    template: '<div class="mini-map"></div>',
     scope: {
       center: '='
     },
-    link($scope, element) {
-      $timeout(initMap, 2000);
+    link(scope, element) {
+      $timeout(initMap, 1000);
 
       function initMap() {
+        console.log(scope.center);
         const map = new $window.google.maps.Map(element[0], {
           zoom: 13,
-          center: $scope.center,
+          center: scope.center,
           scrollwheel: false,
           draggable: true
         });
 
         new $window.google.maps.Marker({
-          position: $scope.center,
+          position: scope.center,
           map: map,
           animation: $window.google.maps.Animation.DROP,
           icon: 'images/blue_pointer.png'
         });
+
+        var panorama = new $window.google.maps.StreetViewPanorama(
+          document.getElementById('pano'),
+          {
+            position: scope.center,
+            pov: {
+              heading: 34,
+              pitch: 10
+            }
+          }
+        );
+        map.setStreetView(panorama);
       }
     }
   };
